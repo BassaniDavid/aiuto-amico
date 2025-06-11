97ccb0fcd58c027d021d6153f2f2c7c90b6e7677
BassaniDavid <davbassan98@gmail.com>
2025-06-11 17:57:44 +0200

    Refactor API key handling and update fetch function for improved clarity and functionality

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
-const API_KEY = "aggiungi la tua key qui"
+const API_KEY = "inserisci qui la tua API KEY"
+const submitButton = document.querySelector('#submit');
+const outputElement = document.querySelector('#output');
+const inputElement =document.querySelector('input');
 
-async function fetchData() {
-    const response = await fetch("https://api.opeain.com/v1/chat/completions", {
-        method: "POST",
+async function getMessage() {
+    console.log("clicked")
+    const options = {
+        method: 'POST',
         headers: {
-            Authorization: `bearer ${API_KEY}`,
-            "Content-Type": "application/json"
+            'Authorization': `Bearer ${API_KEY}`,
+            'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-            model: "gpt-4",
+        model: "gpt-4o-mini",
+        messages: [{role: "user",content: "hello!"}],
+        max_tokens:100
         })
-    })
-    const data = await response.json()
-    console.log(data)
+    }
+    try{
+        const response = await fetch('https://api.openai.com/v1/chat/completions', options)
+        const data = await response.json()
+        console.log(data)
+        outputElement.textContent= data.choices[0].message.content
+    } catch (error){
+        console.error(error)
+    }
+
 }
 
-fetchData()
+submitButton.addEventListener('click', getMessage);
